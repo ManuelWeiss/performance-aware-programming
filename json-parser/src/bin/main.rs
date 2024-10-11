b22d72b14e751dffab21a1ee81e9e66042c9ae79
Manuel Weiss <mweiss@seek.com.au>
2024-10-11 16:56:02 +1000

    try memory-mapping the file

--- json-parser/src/bin/main.rs
+++ json-parser/src/bin/main.rs
@@ -1,17 +1,17 @@
 use std::fs::File;
-use std::io::{BufReader, Read};
 use std::time::Instant;
+use memmap2::Mmap;
 
 use json_parser::parse_input;
 
 fn main() {
     let start_time = Instant::now();
 
-    let filename = "10-point-pairs.json";
+    let filename = "10M-point-pairs.json";
     let file = File::open(filename).expect("Unable to open file");
-    let buffered_reader = BufReader::with_capacity(4 * 1 << 20, file); // buffer size = 4MB, doesn't seem to make a difference
+    let mmap = unsafe { Mmap::map(&file).unwrap() };
 
-    let mut it = buffered_reader.bytes().map(|c| std::char::from_u32(c.unwrap() as u32).unwrap());
+    let mut it = mmap.iter().map(|c| std::char::from_u32(*c as u32).unwrap());
     let sum = parse_input(&mut it);
 
     println!("Sum: {}", sum);
